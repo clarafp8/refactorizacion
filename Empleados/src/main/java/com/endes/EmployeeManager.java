67c04c0ce05729f4359a08e46b7bb419df746c48
clarafp8 <158521872+clarafp8@users.noreply.github.com>
2025-02-11 12:59:08 +0100

    Relleno la clase EmployeeManager.java

--- Empleados/src/main/java/com/endes/EmployeeManager.java
+++ Empleados/src/main/java/com/endes/EmployeeManager.java
@@ -1,5 +1,22 @@
 package com.endes;
-
+import java.util.ArrayList;
+import java.util.List;
 public class EmployeeManager {
-
+  
+  List<Employee> employees = new ArrayList<Employee>();
+    
+    public void addEmployee(Employee e) {
+        if (employees.size()< 10) {
+        	employees.add(e);
+            System.out.println(e + " added to the system.");
+        } else {
+            System.out.println("Cannot add more employees, system is full.");
+        }
+    }
+    
+    @Override
+	public String toString() {
+		return "List of employees: " + employees;
+	}
+}
 }
