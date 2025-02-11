1f42e4e195853673e2a0475772db74e6ade9cf6d
clarafp8 <158521872+clarafp8@users.noreply.github.com>
2025-02-11 12:58:07 +0100

    Relleno la clase Employee.java

--- Empleados/src/main/java/com/endes/Employee.java
+++ Empleados/src/main/java/com/endes/Employee.java
@@ -1,5 +1,35 @@
 package com.endes;
 
 public class Employee {
+	private String name;
+	private int employeeYears;
+	
+	public Employee (String name, int employeeYears) {
+		this.name=name;
+		this.employeeYears=employeeYears;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEmployeeYears() {
+		return employeeYears;
+	}
+
+	public void setEmployeeYears(int employeeYears) {
+		this.employeeYears = employeeYears;
+	}
+
+	@Override
+	public String toString() {
+		return "[Name=" + name + ", EmployeeYears=" + employeeYears + "]";
+	}
+	
+  }
 
 }
